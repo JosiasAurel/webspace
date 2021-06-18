ba401eb71aa39ebae9e046191bdf4a38dd33f109
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-18 03:27:45 +0100

    move to fastapi

---
+++ content/sample.md
@@ -0,0 +1,3 @@
+## This is a sample page
+
+Hello world from my sample page
--- server.py
+++ server.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template, url_for
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from markdown import markdown
+import mistune
 
-app = Flask(__name__)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# set templates
+templates = Jinja2Templates(directory="templates")
 
 
-@app.route("/")
-def handle_index():
-    return render_template("index.html")
+@app.get("/", response_class=HTMLResponse)
+def handle_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.get("/about", response_class=HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
 
 
-# app.run(host="0.0.0.0")
+@app.get("/post/{post}")
+async def handle_post(request: Request, post: str):
+    _content = open(f"content/{post}.md")
+    content = markdown(_content.read())
+    _content.close()
+    return templates.TemplateResponse("post.html", {"request": request, "content": content})
