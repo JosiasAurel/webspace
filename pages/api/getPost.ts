c424b672562ac8295b8de7a31fc5902f090bfcbb
JosiasAurel <ndjosiasaurel@gmail.com>
2022-03-20 12:49:55 +0100

    create api fn to handler fetching a blog post

---
+++ pages/api/getPost.ts
@@ -0,0 +1,24 @@
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { Deta } from "deta";
+
+const deta = Deta(process.env.NEXT_PUBLIC_DETA_PROJECT_KEYs);
+
+const posts = deta.Base("posts");
+
+export async function getPostHandler(req: NextApiRequest, res: NextApiResponse) {
+    const { postURI } = req.body;
+    try {
+        const post = await posts.get(postURI);
+        return res.json({
+            status: true,
+            content: post
+        });
+
+    } catch(err) {
+        return res.json({
+            status: false
+        });
+    }
+}
