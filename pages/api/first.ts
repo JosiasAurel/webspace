7cddfe6c3e5489f625c2edcc104211b82879c92b
JosiasAurel <ndjosiasaurel@gmail.com>
2022-03-20 13:55:31 +0100

    fix: return valid status

---
+++ pages/api/first.ts
@@ -0,0 +1,31 @@
+
+import {
+    NextApiRequest,
+    NextApiResponse
+} from "next";
+import { Deta } from "deta";
+
+const deta = Deta(process.env.NEXT_PUBLIC_DETA_PROJECT_KEY);
+
+const posts = deta.Base("posts");
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const item = posts.put({
+            title: "Hello World",
+            publishDate: new Date().toDateString(),
+            content: "Hello World",
+            views: 1,
+            url: "hello-world"
+        } as Post);
+
+        return res.json({
+            status: true,
+            item
+        });
+    } catch(err) {
+        return res.json({
+            status: false
+        })
+    }
+}
--- pages/api/getPost.ts
+++ pages/api/getPost.ts
@@ -2,19 +2,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { Deta } from "deta";
+import { INSPECT_MAX_BYTES } from "buffer";
 
 const deta = Deta(process.env.NEXT_PUBLIC_DETA_PROJECT_KEY);
 
 const posts = deta.Base("posts");
 
+type Body = {
+    postURI: string
+}
 export default async function getPostHandler(req: NextApiRequest, res: NextApiResponse) {
-    const { postURI } = req.body;
+    const { postURI }: Body = req.body;
     try {
-        const post = await posts.get(postURI);
-        return res.json({
-            status: true,
-            content: post
-        });
+        const fetchedPosts = await (await posts.fetch(
+                {
+                    url: postURI
+                }
+            )
+        ).items as Post[];
+
+        if (fetchedPosts.length > 0) {
+            return res.json({
+                status: true,
+                post: fetchedPosts[0]
+            });
+        } else {
+            return res.json({
+                status: false
+            })
+        }
 
     } catch(err) {
         return res.json({
