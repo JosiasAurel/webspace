1e7c7ba2bb8a73ad5f4280d131b5afc5a01ee5f2
JosiasAurel <ndjosiasaurel@gmail.com>
2022-05-26 10:57:39 +0100

    re-add profile styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,8 +14,8 @@ const Header: React.FC<Props> = ({ theme, changeTheme, atHome }): JSX.Element =>
         <header className={styles.header}>
             <nav>
                 <h2>{!atHome ? (
-                    <svg onClick={_ => router.back()} width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15 6L9 12L15 18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                    <svg onClick={_ => router.back()} width="24" height="24" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15 6L9 12L15 18" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
                 ) :
                     <Link href="/blog">
