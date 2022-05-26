12280d43c9f595ccef1b7b5b5fc01b804ae4e76b
JosiasAurel <ndjosiasaurel@gmail.com>
2022-05-26 10:33:11 +0100

    back arrow goes back in history

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import styles from "../styles/components.module.css";
+import { useRouter } from "next/router";
 
 type Props = {
     theme: string,
@@ -8,15 +9,14 @@ type Props = {
     atHome: boolean
 }
 const Header: React.FC<Props> = ({ theme, changeTheme, atHome }): JSX.Element => {
+    const router = useRouter();
     return (
         <header className={styles.header}>
             <nav>
                 <h2>{!atHome ? (
-                    <Link href="/">
-                        <svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M15 6L9 12L15 18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </Link>
+                    <svg onClick={_ => router.back()} width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15 6L9 12L15 18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
                 ) :
                     <Link href="/blog">
                         <a style={{ color: "black", fontWeight: "lighter" }}>articles</a>
