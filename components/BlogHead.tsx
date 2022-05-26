7a2ee156bb8f4ddc4f0798f638f7f681834e3bef
JosiasAurel <ndjosiasaurel@gmail.com>
2022-05-26 12:09:08 +0100

    add meta tags

--- components/BlogHead.tsx
+++ components/BlogHead.tsx
@@ -4,6 +4,8 @@ import Title from "./Title";
 import Text from "./Text";
 import styles from "../styles/components.module.css";
 
+import Head from "next/head";
+
 type Props = {
     title: string
     description: string
@@ -11,8 +13,18 @@ type Props = {
 }
 
 const BlogHead: React.FC<Props> = ({ title, description, date }): JSX.Element => {
+    const [url, setUrl] = React.useState<string>("");
+    React.useEffect(() => {
+        setUrl(location.href);
+        console.log(url);
+    }, [url]);
     return (
         <div className={styles.blogHead}>
+            {/* affect head from this component */}
+            <Head>
+                <meta property="og:url" content={url} />
+                <meta property="twitter:url" content={url} />
+            </Head>
             <Title>
                 {title}
             </Title>
