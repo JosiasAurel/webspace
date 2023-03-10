25fb733b5b3e84afa93d4f1bcbb1b5439e536190
JosiasAurel <ndjosiasaurel@gmail.com>
2023-03-10 13:32:16 +0100

    integrate splitbee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,14 @@ import "../styles/global.css";
 import "../styles/github-syntax.css";
 import Automata from "../components/Automata";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 const JosiasWDevApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <div className="app">
+      <Head>
+        <script async src="https://cdn.splitbee.io/sb.js"></script>
+      </Head>
       <div>
         <Component {...pageProps} />
         <Footer />
