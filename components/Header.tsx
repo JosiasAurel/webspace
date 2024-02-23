a1f3069bb90f5e755c7b8072fd6f2d220e5c5503
JosiasAurel <ndjosiasaurel@gmail.com>
2024-02-23 01:04:20 +0100

    delete automata and make some improvements to blog head

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,9 @@ import Link from "next/link";
 const Nav: React.FC = (): JSX.Element => {
   return (
     <header>
-      <h2>Birb!</h2>
+      <Link style={{ textDecoration: "none", color: "black"}} href="/">
+        <h2>Birb!</h2>
+      </Link>
       <nav>
         <Link href="/writing">Writing</Link>
         <a href="https://twitter.com/JosiasWing">Twitter</a>
