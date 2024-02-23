a9c5b8f520c279ff49cd2e9ee9ce5a973186acb2
JosiasAurel <ndjosiasaurel@gmail.com>
2024-02-23 10:16:09 +0100

    text fixes and add colors

--- constants/projects.ts
+++ constants/projects.ts
@@ -1,9 +1,20 @@
-export const projects = [
+export type WorkType = "Personal" | "Contributor"
+
+export type Project = {
+  name: string
+  link: string
+  source: string
+  workType: WorkType
+  description: string
+  image: string
+}
+
+export const projects: Array<Project> = [
 {
-    name: "digital art",
+    name: "Digital Art",
     link: "#",
     source: "#",
-    workType: "Digital Math Art",
+    workType: "Personal",
     description:
       "I like to create digital art using maths. This art was created on an 80x60 grid by manipulating the x and y axes as well as using time and positivity as parameters.",
     image: "/marth.png"
@@ -12,7 +23,7 @@ export const projects = [
     name: "sudoku solver",
     link: "https://github.com/JosiasAurel/sudoku-solver",
     source: "https://github.com/JosiasAurel/sudoku-solver",
-    workType: "A brute-force sudoku solver using backtracking",
+    workType: "Personal",
     description:
       "A fast and efficient sudoku solver written in Zig. This sudoku solver performs zero memory allocations and uses backtracking to solve sudoku games.",
     image: "/sudokus.jpg"
@@ -21,7 +32,7 @@ export const projects = [
     name: "c-md-parser",
     link: "https://github.com/JosiasAurel/c-md-parser",
     source: "https://github.com/JosiasAurel/c-md-parser",
-    workType: "A markdown to html converter",
+    workType:"Personal",
     description:
       "A basic markdown to html converter written in pure C. It can also be compiled to WebAssembly and ran in a web browser. It supports only headings, blockquotes, lists, strikethrough, italic and bold text.",
     image: "cmdparser.jpg"
@@ -30,8 +41,7 @@ export const projects = [
     name: "SineRider",
     link: "https://sinerider.com/",
     source: "https://github.com/hackclub/sinerider",
-    workType:
-      "SineRider is a game about love and graphing, built by a global team of teenagers at Hack Club.",
+    workType: "Contributor",
     description:
       "SineRider immerses you into the beauty of mathematics, along with a love story. A special game that recalls the playful side of you, reminding us that we can learn a lot from mathematics and ourselves by just playing a great game.",
     image: "sinerider.jpg"
@@ -41,7 +51,7 @@ export const projects = [
     name: "Buffer",
     link: "https://buffered.josiasw.dev/",
     source: "https://github.com/JosiasAurel/buffer",
-    workType: "A tool to share short text and links across devices.",
+    workType: "Personal",
     description:
       "Buffer is a tool that allows me to share text and links across devices. I built it because I found myself sharing short text between my phone and computer a lot.",
     image: "buffered.jpg"
@@ -50,7 +60,7 @@ export const projects = [
     name: "LinksBook [discontinued]",
     link: "https://linksbook.vercel.app/",
     source: "https://github.com/JosiasAurel/linksbook",
-    workType: "A full-featured bookmark manager",
+    workType: "Personal",
     description:
       "A full-featured bookmark manager written. The project was built over a period of two months so that I could manage my articles (save, read, write summaries, remind myself via email etc) but was later turned into a product that I stopped maintaining.",
     image: "linksbook.jpg"
