52cc4736036964f4db6de6939725ef4b7aea99b2
Josias Aurel <ndjosiasaurel@gmail.com>
2025-10-01 19:05:26 +0100

    attempt fix build

--- next.config.js
+++ next.config.js
@@ -2,37 +2,11 @@
 const nextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: '10mb'
-    }
-  }
-};
-
-const remarkEmbedder = fauxRemarkEmbedder.default;
-const oembedTransformer = fauxOembedTransformer.default;
-
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [
-      remarkMath,
-      [remarkEmbedder, { transformers: [oembedTransformer] }],
-      remarkGfm,
-    ],
-    rehypePlugins: [rehypeMathjax, rehypeHighlight],
-    providerImportSource: "@mdx-js/react",
+      bodySizeLimit: '10mb',
+    },
   },
-});
-
-export default withMDX({
-  output: 'export',
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-  pageExtensions: ["md", "mdx", "tsx", "ts", "jsx", "js"],
   outputFileTracing: true,
   swcMinify: true,
-});
+};
 
-/* module.exports = {
-    outputFileTracing: true,
-    swcMinify: true
-} */
+module.exports = nextConfig;
